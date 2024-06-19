a4b7de9f1b94fa732e3687d5883da373e116cca0
Kazim Shabbir <59815134+Kazim786@users.noreply.github.com>
2024-06-19 11:15:45 -0500

    Update 100_days_of_code.py

--- 100_days_of_code.py
+++ 100_days_of_code.py
@@ -28,50 +28,52 @@
 #Password Generator Project
 
 
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-#I added this bit of code not Angela. 
-random.shuffle(letters)
-random.shuffle(numbers)
-random.shuffle(symbols)
-
-
-
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-
-random_letters = []
-random_symbols = []
-random_numbers = []
-#concatenate values from above 3 to the PW list by using + 
-pw = []
-
+# #I added this bit of code not Angela. 
 # random.shuffle(letters)
-
-# print(letters)
-
-for i in range(nr_letters):
-    random_letters.append(letters.pop())
+# random.shuffle(numbers)
+# random.shuffle(symbols)
 
 
-for i in range(nr_symbols):
-    random_symbols.append(symbols.pop())
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
-for i in range(nr_numbers):
-    random_numbers.append(numbers.pop())
+# random_letters = []
+# random_symbols = []
+# random_numbers = []
+# #concatenate values from above 3 to the PW list by using + 
+# pw = []
 
-pw += random_numbers + random_letters + random_symbols
+# # random.shuffle(letters)
 
-random.shuffle(pw)
+# # print(letters)
 
-print(pw)
+# for i in range(nr_letters):
+#     random_letters.append(letters.pop())
+
+
+# for i in range(nr_symbols):
+#     random_symbols.append(symbols.pop())
+
+
+# for i in range(nr_numbers):
+#     random_numbers.append(numbers.pop())
+
+# pw += random_numbers + random_letters + random_symbols
+
+# random.shuffle(pw)
+
+# pw_str = str(pw)
+
+# print(str(pw_str))
 
 
 #Hard Level - Order of characters randomised:
@@ -81,4 +83,127 @@ print(pw)
 
 
 
-#Day 3
+#Day 7 hangman game 
+
+#Step 1 
+
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+
+
+lives = 6
+
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+display = []
+
+idx = 0
+
+end_of_game = False
+
+
+for letter in chosen_word:
+    display.append("_")
+
+while not end_of_game:
+
+    user_guess = input("Guess a letter: ").lower()
+
+    for letter in chosen_word:
+        if user_guess == letter:
+            print("Right")
+        else:
+            print("Wrong") 
+    
+    #using enumerate
+    for index, letter in enumerate(chosen_word): 
+            if letter == user_guess:
+                display[index] = letter
+                
+        #remember that in the list basics. To assign a value to a certain part of a list you can do it in this way
+        #example_list = ["A", "C", "E"]
+        #example_list[0] = "B"
+        #print(example_list) -> ["B", "C", "E"]
+
+            else:
+                 lives -= 1
+                 if lives == 0:
+                      print("You lose")
+                      end_of_game = True
+    
+
+    print(display)
+
+    if "_" not in display:
+         end_of_game = True
+         print("You win")
+
+
+
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' 
+# has no more blanks ("_"). Then you can tell the user they've won.
